0ad75486a0ac4f7ae441a5ad88257eff8b269c04
jericonb <jnbagadiong@gmail.com>
2023-11-01 09:32:27 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors(
+		{
+			origin: ["https://croffle-haus.vercel.app/"],
+			methods: ["POST", "GET"],
+			credentials: true
+		}
+));
 
 // Static images
 app.use('/Images', express.static('./Images'))
