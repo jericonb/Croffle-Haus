55742f1fdfbed7b9fecddb113e3bd5a326e16494
jericonb <jnbagadiong@gmail.com>
2023-11-01 13:10:15 +0800

    2nd Commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors")
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
-require('dotenv').config();
 
 // Environment Setup
 const port = 4002;
